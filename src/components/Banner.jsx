b746b01c3ed624ae4c4a79d7fe191502a6e59898
Ramen Biswas <printrb72@gmail.com>
2024-11-25 08:08:12 +0600

    Update banner part at 25-11-2024_0808.

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,19 @@ import {Fragment} from "react";
 const Banner = () => {
     return (
         <Fragment>
-            <h1>banner</h1>
+            <section id="banner">
+                <div className="container">
+                    <div className="dFlex">
+                        <div className="bannerLeft">
+                            <p className="sectionSubTitle">I AM DESIGNER</p>
+                            <p className="sectionTitle">Creative Design and Web Solutions</p>
+                        </div>
+                        <div className="bannerRight">
+                            <img src="../../public/images/bannerImage.svg" alt="BannerImage" width="600" height="auto"/>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </Fragment>
     );
 };
--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -6,7 +6,17 @@ const NavBar = () => {
             <nav id="nav">
                 <div className="container">
                     <div className="navWrapper">
-                        <img src="./public/images/Logo.svg" alt="Logo"/>
+                        <img src="./public/images/Logo.svg" alt="Logo" width="135" height="auto"/>
+                        <ul className="dFlex">
+                            <li><a href="../../pages/HomePage.jsx">Home</a></li>
+                            <li><a href="#">about</a></li>
+                            <li><a href="#">Services</a></li>
+                            <li><a href="#">Resume</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">Testimonial</a></li>
+                            <li><a href="#">blog</a></li>
+                        </ul>
+                        <a href="#" className="navBtn">Contact Us</a>
                     </div>
                 </div>
             </nav>
